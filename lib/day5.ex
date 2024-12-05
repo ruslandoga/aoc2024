b7e88f80aa400b9df2c24e69873392d40a47ec42
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-12-05 13:02:09 +0700

    day 5

--- lib/day5.ex
+++ lib/day5.ex
@@ -4,49 +4,113 @@ defmodule Day5 do
   """
 
   def input do
-    File.stream!("priv/day5.txt")
+    File.read!("priv/day5.txt")
   end
 
   def example_input do
-    String.split(
-      """
-      """,
-      "\n",
-      trim: true
-    )
+    """
+    47|53
+    97|13
+    97|61
+    97|47
+    75|29
+    61|13
+    75|53
+    29|13
+    97|29
+    53|29
+    61|53
+    97|53
+    61|29
+    47|13
+    75|47
+    97|75
+    47|61
+    75|61
+    47|29
+    75|13
+    53|13
+
+    75,47,61,53,29
+    97,61,53,29,13
+    75,29,13
+    75,97,47,61,53
+    61,13,29
+    97,13,75,29,47
+    """
   end
 
-  defp process_input(input) do
-    Enum.map(input, &Function.identity/1)
+  def process_input(input) do
+    [rules, pages] = String.split(input, "\n\n")
+
+    rules =
+      rules
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [l, r] = String.split(line, "|")
+        {String.to_integer(l), String.to_integer(r)}
+      end)
+
+    pages =
+      pages
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        line |> String.split(",") |> Enum.map(&String.to_integer/1)
+      end)
+
+    {rules, pages}
   end
 
   @doc """
   Solutions:
 
       iex> part1(example_input())
-      []
+      143
 
       iex> part1(input())
-      []
+      7074
 
   """
-  def part1(input \\ input()) do
-    input
-    |> process_input()
+  def part1(input \\ example_input()) do
+    {rules, pages} = process_input(input)
+
+    pages
+    |> Enum.filter(fn page ->
+      page ==
+        Enum.sort_by(page, &Function.identity/1, fn l, r ->
+          Enum.any?(rules, fn {rl, rr} -> rl == l and rr == r end)
+        end)
+    end)
+    |> Enum.map(fn page -> Enum.at(page, div(length(page), 2)) end)
+    |> Enum.sum()
   end
 
   @doc """
   Solutions:
 
       iex> part2(example_input())
-      []
+      123
 
       iex> part2(input())
-      []
+      4828
 
   """
   def part2(input) do
-    input
-    |> process_input()
+    {rules, pages} = process_input(input)
+
+    pages
+    |> Enum.reject(fn page ->
+      page ==
+        Enum.sort_by(page, &Function.identity/1, fn l, r ->
+          Enum.any?(rules, fn {rl, rr} -> rl == l and rr == r end)
+        end)
+    end)
+    |> Enum.map(fn page ->
+      Enum.sort_by(page, &Function.identity/1, fn l, r ->
+        Enum.any?(rules, fn {rl, rr} -> rl == l and rr == r end)
+      end)
+    end)
+    |> Enum.map(fn page -> Enum.at(page, div(length(page), 2)) end)
+    |> Enum.sum()
   end
 end
