1cd78e5ffe2cb601aa215be1881a2f93a8390ab2
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-12-06 12:15:42 +0700

    day6 part1

--- lib/day06.ex
+++ lib/day06.ex
@@ -10,29 +10,81 @@ defmodule Day6 do
   def example_input do
     String.split(
       """
+      ....#.....
+      .........#
+      ..........
+      ..#.......
+      .......#..
+      ..........
+      .#..^.....
+      ........#.
+      #.........
+      ......#...
       """,
       "\n",
       trim: true
     )
   end
 
-  defp process_input(input) do
-    Enum.map(input, &Function.identity/1)
+  def process_input(input) do
+    map =
+      input
+      |> Enum.with_index()
+      |> Enum.reduce(%{}, fn {line, y}, acc ->
+        String.split(line, "", trim: true)
+        |> Enum.with_index()
+        |> Enum.reduce(acc, fn {char, x}, acc -> Map.put(acc, {x, y}, char) end)
+      end)
+
+    {guard, char} = Enum.find(map, fn {_, v} -> v in ["^", "<", ">", "v"] end)
+    {Map.put(map, guard, "."), guard, char}
   end
 
   @doc """
   Solutions:
 
       iex> part1(example_input())
-      []
+      41
 
       iex> part1(input())
-      []
+      5212
 
   """
-  def part1(input \\ input()) do
-    input
-    |> process_input()
+  def part1(input \\ example_input()) do
+    {map, guard, char} = process_input(input)
+    visited = walk(char, guard, map, _visited = [guard])
+
+    visited
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  defp walk(direction, position, map, visited) do
+    next_position = next_position(direction, position)
+
+    case Map.get(map, next_position) do
+      nil -> visited
+      "." -> walk(direction, next_position, map, [position | visited])
+      "#" -> walk(turn_right(direction), position, map, visited)
+    end
+  end
+
+  defp next_position(direction, {x, y}) do
+    case direction do
+      "^" -> {x, y - 1}
+      "<" -> {x - 1, y}
+      ">" -> {x + 1, y}
+      "v" -> {x, y + 1}
+    end
+  end
+
+  defp turn_right(direction) do
+    case direction do
+      "^" -> ">"
+      "<" -> "^"
+      ">" -> "v"
+      "v" -> "<"
+    end
   end
 
   @doc """
@@ -45,7 +97,7 @@ defmodule Day6 do
       []
 
   """
-  def part2(input) do
+  def part2(input \\ example_input()) do
     input
     |> process_input()
   end
