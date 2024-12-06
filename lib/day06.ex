83409f21e26cc3585e13f35aa2383bfc60935b57
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-12-06 13:21:57 +0700

    day6 part2

--- lib/day06.ex
+++ lib/day06.ex
@@ -4,31 +4,27 @@ defmodule Day6 do
   """
 
   def input do
-    File.stream!("priv/day06.txt")
+    File.read!("priv/day06.txt")
   end
 
   def example_input do
-    String.split(
-      """
-      ....#.....
-      .........#
-      ..........
-      ..#.......
-      .......#..
-      ..........
-      .#..^.....
-      ........#.
-      #.........
-      ......#...
-      """,
-      "\n",
-      trim: true
-    )
+    """
+    ....#.....
+    .........#
+    ..........
+    ..#.......
+    .......#..
+    ..........
+    .#..^.....
+    ........#.
+    #.........
+    ......#...
+    """
   end
 
   def process_input(input) do
     map =
-      input
+      String.split(input, "\n", trim: true)
       |> Enum.with_index()
       |> Enum.reduce(%{}, fn {line, y}, acc ->
         String.split(line, "", trim: true)
@@ -63,7 +59,7 @@ defmodule Day6 do
     next_position = next_position(direction, position)
 
     case Map.get(map, next_position) do
-      nil -> visited
+      nil -> [position | visited]
       "." -> walk(direction, next_position, map, [position | visited])
       "#" -> walk(turn_right(direction), position, map, visited)
     end
@@ -91,14 +87,41 @@ defmodule Day6 do
   Solutions:
 
       iex> part2(example_input())
-      []
+      6
 
       iex> part2(input())
-      []
+      1767
 
   """
   def part2(input \\ example_input()) do
-    input
-    |> process_input()
+    {map, guard, char} = process_input(input)
+    visited = walk(char, guard, map, _visited = [guard])
+    uniq_visited = Enum.uniq(visited) -- [guard]
+
+    Enum.reduce(uniq_visited, 0, fn {x, y}, acc ->
+      visited_obstructions = MapSet.new()
+      looped? = looped?(char, guard, Map.put(map, {x, y}, "#"), visited_obstructions)
+      if looped?, do: acc + 1, else: acc
+    end)
+  end
+
+  defp looped?(direction, position, map, visited_obstructions) do
+    next_position = next_position(direction, position)
+
+    case Map.get(map, next_position) do
+      nil ->
+        false
+
+      "." ->
+        looped?(direction, next_position, map, visited_obstructions)
+
+      "#" ->
+        if MapSet.member?(visited_obstructions, {direction, position}) do
+          true
+        else
+          visited_obstructions = MapSet.put(visited_obstructions, {direction, position})
+          looped?(turn_right(direction), position, map, visited_obstructions)
+        end
+    end
   end
 end
